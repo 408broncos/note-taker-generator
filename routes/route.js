b0678fec53876a4f45609b7525365bf0c1d34650
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-04-27 18:48:18 -0700

    add fixed bugs for all files with completed server.js and route.js files

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,45 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const express = require('express');
 
-app.get('/api/notes', (req, res) => res.json(notes));
+const router = express.Router();
 
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    notes.push(newNote);
-    updateDb();
-    console.log(`New note: ${newNote.title}`);
-  return res.json(newNote);
+let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+router.get('/api/notes', (req, res) => {
+  res.json(notes);
 });
+
+router.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  notes.push(newNote);
+  createDb();
+  console.log(`Added new note: ${newNote.title}`);
+  res.json(newNote);
+});
+
+router.get('/api/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id);
+  const note = notes.find(note => note.id === noteId);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).send(`Note with ID ${noteId} not found`);
+  }
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../assets/notes.html'));
+  });
+  
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../assets/index.html'));
+  });
+
+function createDb() {
+  fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), err => {
+    if (err) throw err;
+  });
+}
+
+module.exports = (router);
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const routes = require('./routes/route');
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static('assets'));
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
